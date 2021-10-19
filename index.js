2183c1b5c2dd23405a579135aa504d86779f730d
Michael Orr <mike@glquote.com>
2021-10-18 18:33:55 -0700

    still can't get switch case to fire off thje addEngineer or addIntern functions

--- index.js
+++ index.js
@@ -1,82 +1,152 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+
 
 const managerQuestions = [
     {
         type: "input",
-        message: "What is the team manager's name?"
+        message: "What is the team manager's name?",
         name: "managerName",
     },
     {
         type: "input",
-        message: "What is the team manager's ID?"
+        message: "What is the team manager's ID?",
         name: "managerID",
     },
     {
         type: "input",
-        message: "What is the team manager's email?"
+        message: "What is the team manager's email?",
         name: "managerEmail",
     },
     {
         type: "input",
-        message: "What is the team manager's office number?"
+        message: "What is the team manager's office number?",
         name: "managerOffice",
+    },
+];
+
+const addEmployee = [
+    {
+        type: "list",
+        message: "Would you like to add another employee?",
+        name: "employeeAdd",
+        choices: ["Yes","No"]
     },
     {
         type: "list",
-        message: "Which type of team member would you like to add?"
+        message: "What typoe of employee would you like to add?",
         name: "employeeType",
-        choices: ['Engineer', 'Intern',"I don't want to add anyone else."],
+        choices: ["Engineer","Intern"],
+        when: (answer) => answer.employeeAdd === 'Yes'
     },
 ];
 
 const engineerQuestions = [
     {
         type: "input",
-        message: "What is the engineer's name?"
+        message: "What is the engineer's name?",
         name: "engineerName",
     },
     {
         type: "input",
-        message: "What is the team engineer's ID?"
+        message: "What is the team engineer's ID?",
         name: "engineerID",
     },
     {
         type: "input",
-        message: "What is the engineer's email?"
+        message: "What is the engineer's email?",
         name: "engineerEmail",
     },
     {
         type: "input",
-        message: "What is the engineer's github profile?"
+        message: "What is the engineer's github profile?",
         name: "engineerGithub",
     },
-]
+];
 
 const internQuestions = [
     {
         type: "input",
-        message: "What is the intern's name?"
+        message: "What is the intern's name?",
         name: "internName",
     },
     {
         type: "input",
-        message: "What is the intern's ID?"
+        message: "What is the intern's ID?",
         name: "internID",
     },
     {
         type: "input",
-        message: "What is the intern's email?"
+        message: "What is the intern's email?",
         name: "internEmail",
     },
     {
         type: "input",
-        message: "What schoot?"
+        message: "What schoot?",
         name: "internSchool",
     },
-]
+];
+
+
+function addEngineer() {
+    inquirer.prompt(engineerQuestions)
+    .then((answers) => {
+        console.log("add Engineer function is being called")
+        const newEngineer = new Engineer(
+            answers.engineerID,
+            answers.engineerName,
+            answers.engineerEmail,
+            answers.engineerGithub)
+        addEmployeeQuestion();
+    })
+}
+
+function addIntern() {
+    inquirer.prompt(internQuestions)
+    .then((answers) => {
+        console.log("add Intern function is being called")
+        const newIntern = new Intern(
+            answers.internID,
+            answers.internName,
+            answers.internEmail,
+            answers.internSchool)
+        addEmployeeQuestion();
+    })
+}
+
+
+
+function addEmployeeQuestion() {
+    inquirer.prompt(addEmployee)
+    .then((answer) =>{
+        switch(answer){
+        case (answer.employeeType === "Engineer"):
+        addEngineer();
+        break;
+        case (answer.employeeType === "Intern"):
+        addIntern();
+        break;
+        case (answer.employeeType === "Quit"):
+        }
+    })
+};
 
 function buildTeam() {
-    
+    inquirer.prompt(managerQuestions)
+    .then((answers) => {
+        const newManager = new Manager(
+            answers.managerID,
+            answers.managerName,
+            answers.managerEmail,
+            answers.managerOffice)
+        addEmployeeQuestion();
+
+    })
 }
+
+buildTeam();
